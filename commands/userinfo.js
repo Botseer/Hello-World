d485fdd22b784bcbe7ce3d9f8df802c08cb9ce6a
The_Learner <bsantosh909@gmail.com>
2017-07-05 19:49:05 +0545

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -13,13 +13,13 @@ if (message.mentions.users.size === 0){
 	let ui_name = client.guilds.get(message.guild.id).members.get(tempn);
 	
     	const embed = new Discord.RichEmbed()
-		.setAuthor(ui_name.user.name, ui_name.user.avatarURL)
+		.setAuthor(ui_name.user.username, ui_name.user.avatarURL)
 		.setColor('RANDOM')
 		.addField('Username', ui_name.user.username, true)
 		.addField('Nickname', ui_name.nickname, true)
 		.addField('Created', moment(ui_name.user.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'))
 		.addField('Joined', moment(ui_name.joinedAt).format('dddd, MMMM Do YYYY, h:mm:ss a'))
-		.addField('Discrim', ui_name.user.discriminator, true)
+		.addField('Discrim', `#**${ui_name.user.discriminator}**`)
 		.addField('Status', ui_name.user.presence.status,true)
 		.addField('Game', ui_name.user.presence.game,true)
 		.addField('Bot', ui_name.user.bot,true)
