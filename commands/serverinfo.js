830b4c1158e64f2fc7ca65fb96275f1cae799bc3
The_Learner <bsantosh909@gmail.com>
2017-07-05 18:32:00 +0545

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,14 +6,14 @@ exports.run = (client, message, args) => {
  
 const embed = new Discord.RichEmbed()
 .setAuthor(message.guild.name, message.guild.iconURL)
-.setColor('RANDOM')
-.setDescription('\nHello everyone, This is a server for pokemon lovers and friendly users around.\nYou can share all your idea about pokemon, talk about rom-hacking and also suggest some improvements as well.')
+.setColor(0x1)
+.setDescription('test')
 .addField('SERVER ID', message.guild.id, true)
 .addField('OWNER', message.guild.owner.user.tag, true)
 .addField('ROLES', message.guild.roles.size, true)
 .addField('TOTAL MEMBERS', message.guild.memberCount)
 .addField('MEMBERS ONLINE', message.guild.members.filter(m => ((m.user.presence.status === "online" )||(m.user.presence.status === "dnd" )||(m.user.presence.status === "idle" ))).size)
-.addField('CREATED DATE', moment(message.guild.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a')e)
+.addField('CREATED DATE', moment(message.guild.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'))
 
 message.channel.send({embed});
 }
