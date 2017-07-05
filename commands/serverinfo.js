459f7c6a8db52039e0017babb9b03310c39f92f8
The_Learner <bsantosh909@gmail.com>
2017-07-05 18:34:49 +0545

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,8 +6,8 @@ exports.run = (client, message, args) => {
  
 const embed = new Discord.RichEmbed()
 .setAuthor(message.guild.name, message.guild.iconURL)
-.setColor(0x1)
-.setDescription('test')
+.setColor('RANDOM')
+.setDescription('\nHello everyone, This is a server for pokemon lovers and friendly users around.\nYou can share all your idea about pokemon, talk about rom-hacking and also suggest some improvements as well.')
 .addField('SERVER ID', message.guild.id, true)
 .addField('OWNER', message.guild.owner.user.tag, true)
 .addField('ROLES', message.guild.roles.size, true)
