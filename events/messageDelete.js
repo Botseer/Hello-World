71023b0f7cb9d7bc9a4227a7a621f2f9e94286f8
The_Learner <bsantosh909@gmail.com>
2017-08-14 21:27:13 +0545

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,4 +1,4 @@
 module.exports = async (message) => {
 
-message.guild.channels.get(`305666620796174337`).send(`**${message.author.username}** deleted a message ${message}`).catch(err => console.error(err));
+message.guild.channels.get(`305666620796174337`).send(`**${message.author.username}** deleted a message _${message}_`).catch(err => console.error(err));
 };
