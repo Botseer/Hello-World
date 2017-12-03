159d11debbe519e2d49dbf04f255c77f71929184
Botseer <homeu99@gmail.com>
2017-12-03 09:26:02 +0545

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,5 @@
 const Command = require('../base/Command.js');
-const { inspect } = require('util');
+const util = require('util');
 
 class Eval extends Command {
   constructor(client) {
@@ -14,16 +14,89 @@ class Eval extends Command {
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
-    try {
-			let evaled = eval(code);
-			if (evaled instanceof Promise) evaled = await evaled;
-			if (typeof evaled !== 'string') evaled = inspect(evaled, { depth: 0 });
-			msg.sendCode('js', this.client.methods.util.clean(evaled));
+    let code = args.join(' ');
+    if (!code) return message.util.reply('No code provided!');
+    const evaled = {};
+    const logs = [];
+    const token = this.client.token.split('').join('[^]{0,2}');
+    const rev = this.client.token.split('').reverse().join('[^]{0,2}');
+    const tokenRegex = new RegExp(`${token}|${rev}`, 'g');
+    const cb = '```';
+    
+    const print = (...a) => { // eslint-disable-line no-unused-vars
+      const cleaned = a.map(obj => {
+        if (typeof o !== 'string') obj = util.inspect(obj, { depth: 1 });
+    	  return obj.replace(tokenRegex, '[TOKEN]');
+        });
+
+        if (!evaled.output) {
+          logs.push(...cleaned);
+        return;
+      }
+
+    evaled.output += evaled.output.endsWith('\n') ? cleaned.join(' ') : `\n${cleaned.join(' ')}`;
+    const title = evaled.errored ? '☠\u2000**Error**' : '📤\u2000**Output**';
+
+    if (evaled.output.length + code.length > 1900) evaled.output = 'Output too long.';
+      evaled.message.edit([
+        `📥\u2000**Input**${cb}js`,
+	code,
+	cb,
+	`${title}${cb}js`,
+	evaled.output,
+	cb
+	]);
+      };
+
+		try {
+			let output = eval(code);
+			// eslint-disable-next-line eqeqeq
+			if (output != null && typeof output.then === 'function') output = await output;
+
+			if (typeof output !== 'string') output = util.inspect(output, { depth: 0 });
+			output = `${logs.join('\n')}\n${logs.length && output === 'undefined' ? '' : output}`;
+			output = output.replace(tokenRegex, '[TOKEN]');
+
+			if (output.length + code.length > 1900) output = 'Output too long.';
+
+			const sent = await message.util.send([
+				`📥\u2000**Input**${cb}js`,
+				code,
+				cb,
+				`📤\u2000**Output**${cb}js`,
+				output,
+				cb
+			]);
+
+			evaled.message = sent;
+			evaled.errored = false;
+			evaled.output = output;
+
+			return sent;
 		} catch (err) {
-			message.channel.send(` \`ERROR\`\n${err}`);
-			if (err.stack) message.channel.send(`\`error:\` ${err.stack}`);
+			console.log('An eval error occured.');
+			console.log(err);
+			let error = err;
+
+			error = error.toString();
+			error = `${logs.join('\n')}\n${logs.length && error === 'undefined' ? '' : error}`;
+			error = error.replace(tokenRegex, '[TOKEN]');
+
+			const sent = await message.util.send([
+				`📥\u2000**Input**${cb}js`,
+				code,
+				cb,
+				`☠\u2000**Error**${cb}js`,
+				error,
+				cb
+			]);
+
+			evaled.message = sent;
+			evaled.errored = true;
+			evaled.output = error;
+
+			return sent;
 		}
-  }
 }
 
 module.exports = Eval;
